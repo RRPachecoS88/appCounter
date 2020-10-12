c0fd90e212d473c5a6fb1dd0b5c8744072e804cf
observatorio <raul.pacheco@conalpedis.gob.bo>
2020-10-11 20:06:41 -0400

    Prueba 2 indicar posiciones del cuerpo

--- app/src/main/java/com/example/appcounter/MainActivity.java
+++ app/src/main/java/com/example/appcounter/MainActivity.java
@@ -26,9 +26,31 @@ public class MainActivity extends AppCompatActivity {
         evento = new SensorEventListener(){
             @Override
             public void onSensorChanged(SensorEvent event){
-                System.out.println("X= "+event.values[0]);
-                System.out.println("Y= "+event.values[1]);
-                System.out.println("Z= "+event.values[2]);
+                //System.out.println("X= "+event.values[0]);
+               // System.out.println("Y= "+event.values[1]);
+               // System.out.println("Z= "+event.values[2]);
+                float A=event.values[0];
+                float B=event.values[1];
+                float C=event.values[2];
+                if((A>-1.24  && A<2.59) && (B>3.11 && B<9.75)&&(C>1 && C<8.55)){
+                    System.out.println("Esta de pie");
+                }else if((A>-3.57  && A<1) && (B>0.31 && B<9.75)&&(C>-2.6 && C<9.85)){
+                    System.out.println("Esta con brazos extendidos");
+                }else if((A>-9.67  && A<8.95) && (B>-5.68 && B<0.79)&&(C>-0.79 && C<9.57)){
+                    System.out.println("Esta con brazo flexionados ");
+                }else{
+
+                }
+                /*while((A>-1.24  && A<2.59) && (B>3.11 && B<9.75)&&(C>1 && C<8.55)){
+                    System.out.println("Esta de pie");
+                }
+                while((A>-3.57  && A<1) && (B>0.31 && B<9.75)&&(C>-2.6 && C<9.85)){
+                    System.out.println("Esta con brazos extendidos");
+                }
+                while((A>-9.67  && A<8.95) && (B>-5.68 && B<0.79)&&(C>-0.79 && C<9.57)){
+                    System.out.println("Esta con brazo flexionados ");
+                }*/
+
 
             }
             @Override
